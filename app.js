83b20f9d4ec61b9f919dc3cc4b987193278579a8
remi-code <sushil@gmail.com>
2026-09-20 12:50:31 +0530

    add storeItem

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ app.engine("ejs", ejsMate);
 app.use(express.static(path.join(__dirname, "/public")));
 
 app.get("/", (req,res) => {
-    res.send("Hi, I am root");
+    res.redirect("/goods")
 });
 
 //Index Route
